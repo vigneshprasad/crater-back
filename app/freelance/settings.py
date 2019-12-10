4a01af8a35634a490156f06b37e3e323aecb657f
Anton <amaistrenko@s-pro.io>
2019-12-10 12:29:01 +0200

    Set Allow hosts in settings.

--- app/freelance/settings.py
+++ app/freelance/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 AUTH_USER_MODEL = 'users.User'
