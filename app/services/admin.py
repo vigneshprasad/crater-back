2657662a461bc58b2e7a9255a58885d669a97498
Anton <amaistrenko@s-pro.io>
2019-12-19 11:54:02 +0200

    Admin UI | Services Admin configuration

--- app/services/admin.py
+++ app/services/admin.py
@@ -1,1 +1,27 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+
+from services.models import Category, ServiceType, Service
+from utils.mixins import ViewActionMixin
+
+
+class ServiceTypeInline(admin.TabularInline):
+    model = ServiceType
+    extra = 0
+
+
+@register(Category)
+class CategoryAdmin(ViewActionMixin, ModelAdmin):
+    icon_name = 'apps'
+    list_display = ('name', 'action')
+    inlines = [ServiceTypeInline]
+    search_fields = ['name']
+
+
+@register(Service)
+class ServiceAdmin(ViewActionMixin, ModelAdmin):
+    icon_name = 'room_service'
+    list_display = ('service_type', 'status', 'user', 'action')
+    list_filter = ['status']
+    search_fields = ['user__email', 'service_type__name']
+
--- app/services/apps.py
+++ app/services/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ServicesConfig(AppConfig):
     name = 'services'
+    icon_name = 'room_service'
