839116e0afb82a03fe50b841d0fffb87f3554d0b
Anton <amaistrenko@s-pro.io>
2020-02-10 18:41:44 +0200

    Add chat event GET_SUPPORT_CHAT_MESSAGES.

--- app/consumers/consumers.py
+++ app/consumers/consumers.py
@@ -195,7 +195,7 @@ class ChatConsumer(ChatAuthConsumer):
         messages = await get_paginated_support_messages(self.user_id, page=event.get('page', 1))
         results = json.loads(JSONRenderer().render(messages).decode('utf8'))
         await self.send(text_data=json.dumps({
-            'type': 'get_messages',
+            'type': 'get_support_chat_messages',
             'results': results
         }))
 
@@ -317,7 +317,7 @@ class ChatConsumer(ChatAuthConsumer):
         messages = await get_paginated_support_messages(self.user_id, page=event.get('page', 1))
         results = json.loads(JSONRenderer().render(messages).decode('utf8'))
         await self.send(text_data=json.dumps({
-            'type': 'get_messages',
+            'type': 'get_support_chat_messages',
             'results': results
         }))
 
