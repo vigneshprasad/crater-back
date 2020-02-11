f201d6f7d70014830739b23956511b5533709b11
Anton <amaistrenko@s-pro.io>
2020-02-11 13:26:32 +0200

    Remove closure code for failed socket authentication

--- app/consumers/connect.py
+++ app/consumers/connect.py
@@ -40,7 +40,7 @@ class ChatAuthConsumer(AsyncWebsocketConsumer):
             'message': {'status_code': 401},
             'user': 'anonymous'
         }))
-        await self.close(code=1007)
+        await self.close()
 
     async def disconnect(self, close_code):
         try:
