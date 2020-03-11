a35115868f685bbf10eb47ac9ce948d89c4dbfab
Anton <amaistrenko@s-pro.io>
2020-03-11 10:52:09 +0200

    Enhance pagination fot network and articles

--- app/resources/curated_articles/paginators.py
+++ app/resources/curated_articles/paginators.py
@@ -1,5 +1,17 @@
+from collections import OrderedDict
+
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class CuratedArticlePagination(PageNumberPagination):
     page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('current_page', int(self.request.query_params.get('page', 1))),
+            ('results', data)
+        ]))
