5d9e64e447af4e90c4902e102a014e44062f1d58
Anton <amaistrenko@s-pro.io>
2020-03-11 13:53:29 +0200

    Catch deepcopy error

--- app/community/mixins.py
+++ app/community/mixins.py
@@ -9,7 +9,10 @@ class SetCreatorRequestDataMixin:
         Initial transform data for serializer, set creator as request user
         :param data: request data
         """
-        data = copy.deepcopy(data)
+        try:
+            data = copy.deepcopy(data)
+        except TypeError:
+            pass
         if self.context.get('request'):
             data[self.request_user] = self.context['request'].user.pk
         return super().to_internal_value(data)
