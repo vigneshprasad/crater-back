ba205c923a98588b96db8e9de4cde6045559a6d2
vskliar <vskliar@s-pro.io>
2020-04-21 14:06:49 +0300

    Remove validation email verify

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -109,9 +109,9 @@ class LoginSerializer(rest_auth_serializers.LoginSerializer):
             if not user.is_active:
                 msg = _('User account is disabled.')
                 raise exceptions.ValidationError(msg)
-            if user.emailaddress_set.exists() and not user.emailaddress_set.first().verified:
-                msg = _('Please  confirm your e-mail first.')
-                raise exceptions.ValidationError(msg)
+            # if user.emailaddress_set.exists() and not user.emailaddress_set.first().verified:
+            #     msg = _('Please  confirm your e-mail first.')
+            #     raise exceptions.ValidationError(msg)
         else:
             msg = _('Email or password is not correct')
             raise exceptions.ValidationError(msg)
