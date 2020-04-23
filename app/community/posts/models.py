bf6bdc74be6bd2155941dd807b169f8872d0f02e
vskliar <vskliar@s-pro.io>
2020-04-23 17:48:38 +0300

    Fix notification creating

--- app/community/posts/models.py
+++ app/community/posts/models.py
@@ -89,7 +89,7 @@ def post_post_save(sender, instance,  created, *args, **kwargs):
         if instance.group:
             users_approved = list(instance.group.group_users.filter(is_approved=True).values_list('user_id', flat=True))
         else:
-            users_approved = list(instance.creator.followers.all().values_list('followed_id', flat=True))
+            users_approved = list(instance.creator.follows.values_list('follower_id', flat=True))
         users = User.objects.filter(pk__in=users_approved, profile__isnull=False).exclude(pk=instance.creator.pk)
         for user in users:
             UserNotification.objects.create(user=user, notification=notification)
