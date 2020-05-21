3f73b40f4f8bd57a8b0b396bb5d21869f2374163
vskliar <vskliar@s-pro.io>
2020-05-21 12:04:58 +0300

    Try new python server

--- docker/start.sh
+++ docker/start.sh
@@ -12,5 +12,6 @@ done;
 ./manage.py collectstatic --no-input --traceback
 #./manage.py makemessages --locale=ru --extension=html,txt,py --ignore=venv
 #./manage.py compilemessages --locale=ru
-daphne -b 0.0.0.0 -p 8000 freelance.asgi:application
+# daphne -b 0.0.0.0 -p 8000 freelance.asgi:application
 #./manage.py runserver 0.0.0.0:8000
+uvicorn --host 0.0.0.0 --port 8000 --loop asyncio --http h11 freelance.asgi:application --workers 4
