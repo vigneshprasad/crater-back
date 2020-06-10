c046ea2f1e6c5eacaf26f547de8361d698d32e5b
vskliar <vskliar@s-pro.io>
2020-06-10 10:56:32 +0300

    Apple adapter fixes

--- app/users/appleid/views.py
+++ app/users/appleid/views.py
@@ -1,10 +1,12 @@
 import jwt
 import requests
+from allauth.account.utils import user_email, user_field, user_username
 from allauth.socialaccount.providers.oauth2.views import (
     OAuth2Adapter,
     OAuth2CallbackView,
     OAuth2LoginView,
 )
+from allauth.utils import valid_email_or_none
 from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
@@ -43,7 +45,10 @@ class AppleOAuth2Adapter(OAuth2Adapter):
         id_token = response_dict.get('id_token', None)
         if id_token:
             decoded = jwt.decode(id_token, '', verify=False)
-            response_data.update({'email': decoded['email']}) if 'email' in decoded else None
+            if 'email' in decoded:
+                response_data.update({'email': decoded['email']})
+            else:
+                response_data.update({'email': None})
             response_data.update({'uid': decoded['sub']}) if 'sub' in decoded else None
         else:
             raise serializers.ValidationError(
@@ -75,6 +80,36 @@ class AppleOAuth2Adapter(OAuth2Adapter):
             headers=headers
         ).decode("utf-8")
         return apple_client_id, client_secret
+
+    def populate_user(self,
+                      request,
+                      sociallogin,
+                      data):
+        """
+        Hook that can be used to further populate the user instance.
+
+        For convenience, we populate several common fields.
+
+        Note that the user instance being populated represents a
+        suggested User instance that represents the social user that is
+        in the process of being logged in.
+
+        The User instance need not be completely valid and conflict
+        free. For example, verifying whether or not the username
+        already exists, is not a responsibility.
+        """
+        username = data.get('username')
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        name = data.get('name')
+        user = sociallogin.user
+        user_username(user, username or '')
+        user_email(user, valid_email_or_none(email))
+        name_parts = (name or '').partition(' ')
+        user_field(user, 'first_name', first_name or name_parts[0])
+        user_field(user, 'last_name', last_name or name_parts[2])
+        return user
 
 
 oauth2_login = OAuth2LoginView.adapter_view(AppleOAuth2Adapter)
