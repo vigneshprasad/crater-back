f64e690d697457308c10dab465723932035d4776
vskliar <vskliar@s-pro.io>
2020-06-10 13:54:27 +0300

    Rewrite user authentication check

--- app/freelance/settings.py
+++ app/freelance/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
         # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
-        'user.authentication.JSONWebTokenAuthentication',
+        'users.authentication.JSONWebTokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S.%fZ",
