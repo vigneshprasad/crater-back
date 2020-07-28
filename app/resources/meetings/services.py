851086564624d70a2f5cb6b4d863415e438641a2
Nishant Parashar <nishant@worknetwork.in>
2020-07-28 17:58:39 +0530

    Doc String and minor changes to tasks

--- app/resources/meetings/services.py
+++ app/resources/meetings/services.py
@@ -23,33 +23,68 @@ def get_interest_list():
 
 
 def create_meetings_for_time_period(
-        stat_date,
+        start_date,
         end_date,
         title=choices.DEFAULT_MEETING_TITLE,
         time_slots=None,
         registration_end_date=None
 ):
-    if not time_slots:
-        return
+    """
+    Creates meeting object for a give time period.
 
+    Args:
+        start_date(Date): Meeting start date.
+        end_date(Date): Meeting end date.
+        title(Char): Meeting title. Not required since
+            default is provided.
+        time_slots(list(TimeSlot)): List of TimeSlots objects
+            for the meeting. If not provided these are created
+            with a set of default time slots.
+        registration_end_date(Date): Meeting registration closing
+            date.
+
+    Returns:
+        Created Meeting object.
+
+    """
+    if not time_slots:
+        time_slots = create_default_time_slots(start_date, end_date)
+
+    # If registration end date is not present, week end
+    # is taken as default registration end date.
     if not registration_end_date:
         registration_end_date = end_date
 
-    meeting = models.Meeting.objects.create(
-        week_start_date=stat_date,
+    meeting, _ = models.Meeting.objects.get_or_create(
+        week_start_date=start_date,
         week_end_date=end_date,
         registration_end_date=registration_end_date,
         title=title
     )
-    meeting.time_slots.add(time_slots)
+    # Added time slots to the meeting object.
+    meeting.available_time_slots.add(*time_slots)
+
+    return meeting
 
 
 def create_default_time_slots(start_date, end_date):
+    """
+    Create default time slots for given start and end dates.
+
+    Args:
+        start_date(Date)
+        end_date(Date)
+
+    Returns:
+        time_slots(list(TimeSlot)): List of time slots created
+            for the given start and end dates.
+
+    """
 
     all_time_slots = []
     date = start_date
 
-    while date < end_date:
+    while date <= end_date:
 
         weekday = date.weekday()
         time_slots_for_weekday = choices.DEFAULT_TIME_SLOTS.get(weekday)
@@ -67,10 +102,30 @@ def create_default_time_slots(start_date, end_date):
 
 
 def create_time_slots_for_date_and_slots(date, time_slots):
+    """
+    Create time slots for a particular date and time slots
+    info.
+
+    Args:
+        date(Date): Date for which time slots are being
+            created.
+        time_slots(list(dict)): List of dictionaries with
+            start_time and end_time.
+            [{
+                'start_time': time(12, 00, 00),
+                'end_time': time(12, 30, 00)
+            }]
+
+    Returns:
+        slots(list(TimeSlots)): Returns a list of time slots
+            created.
+
+    """
     slots = []
 
     for time_slot in time_slots:
-        slot = models.TimeSlot.objects.create(
+
+        slot, _ = models.TimeSlot.objects.get_or_create(
                 date=date,
                 start_time=time_slot['start_time'],
                 end_time=time_slot['end_time']
@@ -81,6 +136,14 @@ def create_time_slots_for_date_and_slots(date, time_slots):
 
 
 def get_old_active_meetings():
+    """
+    Closes meetings based on info provided
+    in the model. (week_end_date)
+
+    return:
+        Queryset of meetings.
+
+    """
     return models.Meeting.objects.filter(
         is_active=True,
         week_end_date__lt=timezone.now().date()
@@ -88,7 +151,15 @@ def get_old_active_meetings():
 
 
 def get_meetings_with_open_registration():
+    """
+    Closes registration for meeting based on info provided
+    in the model. (registration_end_date)
+
+    return:
+        Queryset of meetings.
+
+    """
     return models.Meeting.objects.filter(
         is_registration_opne=True,
-        week_end_date__lt=timezone.now().date()
+        registration_end_date__lt=timezone.now().date()
     )
--- app/resources/meetings/tasks.py
+++ app/resources/meetings/tasks.py
@@ -1,40 +1,72 @@
 import datetime
 
 from celery.schedules import crontab
+from celery.task import periodic_task
 from django.utils import timezone
 
-from freelance.celery import app
 from resources.meetings import services
 from resources.meetings import choices
 
 
-@app.task(crontab(day_of_week='sunday', hour='0', minute='0'))
-def create_weekly_one_on_one_meeting():
-    week_start_date = timezone.now().date()
-    week_end_date = week_start_date + datetime.timedelta(days=6)
+@periodic_task(run_every=crontab(day_of_week='sunday', hour='19', minute='00'))
+def create_weekly_one_on_one_meeting(time_slots=None):
+    """
+    Creates weekly meeting and default time slots for that
+    meeting.
 
-    new_time_slots = services.create_default_time_slots(week_start_date, week_end_date)
+    Args:
+        time_slots(list(TimeSlots)): List of TimeSlots objects to
+            be associated with the meeting. If not provided
+            default time slots will be created.
 
+    Notes:
+        week_start_date will be that week's Monday.
+        week_end_date will be that week's Saturday.
+        registration_end_date(default) will be that
+            week's Wednesday
+
+    """
     title = choices.DEFAULT_ONE_ON_ONE_MEETING_TITLE
-    registration_end_date = week_start_date + datetime.timedelta(days=4)
+    # timezone.now() will provide Sunday's date. Since both are UTC.
+    week_start_date = timezone.now().date() + datetime.timedelta(days=1)
+    week_end_date = week_start_date + datetime.timedelta(days=5)
+
+    registration_end_date = week_start_date + datetime.timedelta(
+        days=choices.DEFAULT_REGISTRATION_CLOSED_WEEKDAY
+    )
+
     services.create_meetings_for_time_period(
         week_start_date,
         week_end_date,
-        time_slots=new_time_slots,
+        time_slots=time_slots,
         registration_end_date=registration_end_date,
         title=title
     )
 
 
-@app.task(crontab(hour='0', minute='0'))
+@periodic_task(crontab(hour='0', minute='0'))
 def close_last_weeks_meetings():
+    """
+    Closes meetings with week_end_date less
+    than today.
+
+    Runs everyday at 00:00 hour.
+
+    """
     meetings = services.get_old_active_meetings()
     for meeting in meetings:
         meeting.close_meeting()
 
 
-@app.task(crontab(hour='0', minute='0'))
+@periodic_task(crontab(hour='0', minute='0'))
 def close_registration_for_last_weeks_meetings():
+    """
+    Closes registration for meetings with registration_end_date
+    less than today.
+
+    Runs everyday at 00:00 hour.
+
+    """
     meetings = services.get_meetings_with_open_registration()
     for meeting in meetings:
         meeting.close_registration()
