97b9ece3bb727c60d5225aa2ab14f8e172f21bf7
Abhishek Rajamohan <abhishek.rajamohan@gmail.com>
2020-08-11 16:30:43 +0000

    Signup flow changes phase 1

--- app/users/services.py
+++ app/users/services.py
@@ -42,3 +42,14 @@ def create_or_update_user_device_info(
         defaults={'last_used': timezone.now()}
     )
     return user_device_info, created
+    
+def get_social_account_info(social_account):
+    data = {}
+    if not social_account:
+        return data
+    extra_data = social_account.extra_data
+    if social_account.provider == 'google':
+        data = {
+            'photo_url': extra_data['picture']
+        }
+    return data
