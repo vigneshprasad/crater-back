5d33961d4b39df5eb8d8562c5f8afce969c5be2e
vskliar <vskliar@s-pro.io>
2020-09-14 10:36:13 +0300

    Added has active subscription permission

--- app/payment/admin.py
+++ app/payment/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.admin import register
+
 from payment.models import BankDetails
 
 
 @register(BankDetails)
 class BankDetailsAdmin(admin.ModelAdmin):
     list_display = ('user', 'stripe_customer_id')
-    icon_name='payment'
+    icon_name = 'payment'
 
     def has_add_permission(self, request, obj=None):
         return False
