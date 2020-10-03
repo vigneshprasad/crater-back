4e2705b60e0a7cb942df99814db48ba05f2a5071
Abhishek R <abhishek@worknetwork.in>
2020-10-03 19:45:45 +0530

    created task to send whatsapp meeting time confirmation from meeting objects

--- app/integrations/freshchat/public.py
+++ app/integrations/freshchat/public.py
@@ -1,5 +1,7 @@
 import logging
 
+from datetime import datetime
+
 from integrations.freshchat import constants
 from integrations.freshchat import freshchat_service
 
@@ -33,3 +35,31 @@ def send_meeting_opt_in_messages(users):
             template_name=constants.MEETING_OPT_IN_REMINDER_TEMPLATE,
             template_data=[{"data": user.get_display_first_name()}]
         )
+
+
+def send_meeting_time_confirmation(user, start_time, end_time):
+    """Send meeting time slot confirmation messages.
+
+    Args:
+        user(User): User to whom this message will go.
+        start_time(datetime): Start datetime object for meeting
+        end_time(datetime): End datetime object for meeting
+
+    """
+    logging.info("Send meeting time confirmation for user {} and Time: {} - {}".format(
+        user.email, start_time, end_time
+    ))
+
+    date = start_time.strftime('%a, %d %b %Y')
+    start_time = start_time.strftime('%I:%M %p')
+    end_time = end_time.strftime('%I:%M %p')
+    time = "{} - {}".format(start_time, end_time)
+    freshchat_service.freshchat_whatsapp_service.send_outbound_message(
+        user=user,
+        template_name=constants.MEETING_CONFIRMATION_FRESHCHAT_TEMPLATE,
+        template_data=[
+            {"data": user.name},
+            {"data": time},
+            {"data": date},
+        ]
+    )
