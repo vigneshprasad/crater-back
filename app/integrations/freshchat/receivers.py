4e7f76b804eeb6b21951ee8cc31c2c3bcf34c58c
Vignesh Prasad <vignesh@worknetwork.in>
2020-11-15 01:41:12 +0000

    WNS 107: WhatsApp message changes

--- app/integrations/freshchat/receivers.py
+++ app/integrations/freshchat/receivers.py
@@ -3,6 +3,8 @@ from django.dispatch import receiver
 
 from users import signals as user_signals
 from integrations.freshchat import tasks
+from integrations.freshchat import public
+from resources.meetings import signals as meeting_signals
 
 
 @receiver(user_signals.user_updated)
@@ -18,3 +20,19 @@ def create_or_update_freshchat_user(sender, user, *args, **kwargs):
         return
 
     tasks.create_or_update_freshchat_user.delay(user.pk)
+
+@receiver(meeting_signals.registered_for_meeting)
+def registered_for_meeting(sender, user, **kwargs):
+    """
+        If a user meeting preference is created
+        a whatsapp message is sent to the user 
+        with a confirmation
+    """
+
+    # Removing signal object from kwargs.
+    kwargs.pop('signal')
+
+    created = kwargs.pop('created', None)
+     
+    if created:
+        public.send_registration_confirmation(user)
