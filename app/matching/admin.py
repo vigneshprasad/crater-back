023f13d82da65a283540d487ee7e8a11c9abda5a
Nishant Parashar <nishant@worknetwork.in>
2020-12-07 20:54:19 +0530

    Fixed comments

--- app/matching/admin.py
+++ app/matching/admin.py
@@ -5,5 +5,7 @@ from matching import models
 
 
 @register(models.MatchScore)
-class BankDetailsAdmin(admin.ModelAdmin):
+class MatchScoreAdmin(admin.ModelAdmin):
     list_display = ('user', 'score')
+    search_fields = ('user__email', )
+    exclude = ('created_at', 'deleted_at', 'updated_at', 'is_deleted', 'score_breakdown', 'score_weightages')
--- app/matching/serializers.py
+++ app/matching/serializers.py
@@ -20,12 +20,6 @@ class PublicTopMatchesSerializer(serializers.Serializer):
 
     all_score = serializers.SerializerMethodField()
 
-    # final_score = serializers.SerializerMethodField()
-    # interest_objective_to_tag_score = serializers.SerializerMethodField()
-    # tag_to_tag_score = serializers.SerializerMethodField()
-    # objective_to_objective_score = serializers.SerializerMethodField()
-    # introduction_text_score = serializers.SerializerMethodField()
-
     @staticmethod
     def get_all_score(user):
         return public.get_top_matches_for_user(user)
