5f035ec5f68c2483cca3679448b0f7aa22aabd43
Nishant Parashar <nishant@worknetwork.in>
2021-03-31 18:32:16 +0530

    Added a autmate function

--- app/users/scripts/data_generation_script.py
+++ app/users/scripts/data_generation_script.py
@@ -1,3 +1,6 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
 from users import models
 from resources.meetings import models as meeting_models
 from conversations import models as conversation_models
@@ -6,6 +9,36 @@ from conversations import models as conversation_models
 ORGANIC_BASE_SOURCE_ID = models.BaseSource.objects.get(name="Organic").id
 GOOGLE_BASE_SOURCE_ID = models.BaseSource.objects.get(name="Google").id
 FACEBOOK_BASE_SOURCE_ID = models.BaseSource.objects.get(name="Facebook").id
+
+
+def get_all_month_by_month_data(start):
+    """Print month by month data for given start and end date.
+
+    Args:
+        start(datetime.datetime): Datetime object from where we are starting
+            to calculate monthly metrics.
+
+    """
+    while start < datetime.datetime.now():
+        next_month_start = start + relativedelta(months=+1)
+        end = next_month_start - datetime.timedelta(days=1)
+
+        print("Acquisition Details", "*"*30)
+        print("Organic User Count: ", get_organic_users_count(start=start, end=end))
+        print("Paid User Count: ", get_paid_users_count(start=start, end=end))
+
+        print("Profile Data", "*"*30)
+        print("Tag Distribution of Users: ", get_users_distribution_based_on_tag(start=start, end=end))
+        print("Experience Distribution of Users: ", get_users_distribution_based_on_experience(start=start, end=end))
+        print("Sector Distribution of Users: ", get_users_distribution_based_on_sector(start=start, end=end))
+
+        print("Conversations Metrics: ", "*"*30)
+        print("Total Users Signed up for Conversations: ", get_total_users_signed_up_for_meetings(start=start, end=end))
+        print("Unique Users for 1:1 Meetings: ", get_unique_participants_in_meetings(start=start, end=end))
+        print("Unique Users for Group Meetings: ", get_unique_participants_in_group_meetings(start=start, end=end))
+
+        print("Meetings Count: ", get_retention_of_users(start=start, end=end))
+        start = next_month_start
 
 
 def get_organic_users_count(start=None, end=None):
@@ -154,7 +187,6 @@ def get_number_of_meetings(start=None, end=None):
 
 def get_unique_participants_in_group_meetings(start=None, end=None):
     unique_speakers = set()
-
     if start and end:
         group_meetings = conversation_models.Group.objects.filter(start__gte=start, end__lte=end)
     else:
@@ -175,7 +207,7 @@ def get_number_of_group_meetings(start=None, end=None):
     return group_meetings.count()
 
 
-def get_retention_of_users(start=None, end=None):
+def get_retention_of_users(end=None):
     one = 0
     two = 0
     three = 0
@@ -189,13 +221,18 @@ def get_retention_of_users(start=None, end=None):
     fifteen = 0
     twenty = 0
 
-    user_queryset = models.User.objects.filter(date_joined__gte=start, date_joined__lte=end) if (
-            start and end) else models.User.objects.all()
+    user_queryset = models.User.objects.all()
 
     for user in user_queryset:
 
-        meetings = meeting_models.Meeting.objects.filter(participants=user)
-        group_meetings = conversation_models.Group.objects.filter(speakers=user)
+        meetings = meeting_models.Meeting.objects.filter(
+            participants=user,
+            created_at__lte=end
+        )
+        group_meetings = conversation_models.Group.objects.filter(
+            speakers=user,
+            created_at__lte=end
+        )
 
         total_conversations_count = meetings.count() + group_meetings.count()
 
@@ -224,15 +261,15 @@ def get_retention_of_users(start=None, end=None):
         if total_conversations_count > 19:
             twenty += 1
 
-    print("1+", one)
-    print("2+", two)
-    print("3+", three)
-    print("4+", four)
-    print("5+", five)
-    print("6+", six)
-    print("7+", seven)
-    print("8+", eight)
-    print("9+", nine)
-    print("10+", ten)
-    print("15+", fifteen)
-    print("20+", twenty)
+    print("1+ conversations: ", one)
+    print("2+ conversations: ", two)
+    print("3+ conversations: ", three)
+    print("4+ conversations: ", four)
+    print("5+ conversations: ", five)
+    print("6+  conversations: ", six)
+    print("7+ conversations: ", seven)
+    print("8+ conversations: ", eight)
+    print("9+ conversations: ", nine)
+    print("10+ conversations: ", ten)
+    print("15+ conversations: ", fifteen)
+    print("20+ conversations: ", twenty)
