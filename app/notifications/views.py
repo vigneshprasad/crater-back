bfc4e60f54dfd3eaec40bdd1f627cbc0442f973a
Nishant Parashar <nishant@worknetwork.in>
2021-05-07 16:43:12 +0530

    Removed signal for app start

--- app/notifications/views.py
+++ app/notifications/views.py
@@ -50,11 +50,11 @@ class NotificationViewSet(mixins.ListModelMixin,
         # TODO(Nishant): Will reuse this code when Flutter app is released.
         # This is the call that always happens as you open the app
         # Hence firing the app started signal.
-        signals.app_started_signal.send(
-            sender=None,
-            user=request.user,
-            device_info=request.user_agent
-        )
+        # signals.app_started_signal.send(
+        #     sender=None,
+        #     user=request.user,
+        #     device_info=request.user_agent
+        # )
         return super().list(request, *args, **kwargs)
 
     def get_queryset(self):
