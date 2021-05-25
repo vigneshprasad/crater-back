7e3191240696552ef427d6d7d9c7681452d3d3ec
Vignesh Prasad <vigneshprasad@Vigneshs-MacBook-Pro.local>
2021-05-25 18:15:25 +0530

    Removed inbox

--- app/integrations/google/constants.py
+++ app/integrations/google/constants.py
@@ -20,7 +20,7 @@ CALENDAR_SCOPES = [
 DEFAULT_SUMMARY_FOR_MEETING_EVENTS = "1:1_Professional Networking_WorkNetwork"
 
 DEFAULT_DESCRIPTION_FOR_MEETING_EVENTS = "Hi, your 1:1 meeting has been scheduled for the above time. Please view " \
-                                         "your inbox for an introduction to your match, their profile & the meeting " \
+                                         "the app for an introduction to your match, their profile & the meeting " \
                                          "details (an email from meetings@worknetwork.in would have been received). " \
                                          "In case you need to reschedule your meeting or for any other update, " \
                                          "please send an email on that thread to all participants."
