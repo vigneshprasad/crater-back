0bbb6a5e6e1e898744b0020ddf0d8b1b86d993a9
Nishant Parashar <nishant@worknetwork.in>
2021-08-30 13:01:10 +0530

    Refractored code

--- app/users/views.py
+++ app/users/views.py
@@ -42,7 +42,7 @@ from conversations import models as conversation_models
 class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
     post_reset_login = True
     form_class = AdminSetPasswordForm
-    success_url = reverse_lazy('admin:dashboard_dashboard_changelist')
+    success_url = reverse_lazy("admin:dashboard_dashboard_changelist")
 
 
 class ProfileViewSet(
@@ -56,13 +56,13 @@ class ProfileViewSet(
 
     def create(self, request, *args, **kwargs):
         created_flag = True
-        if hasattr(request.user, 'profile') and request.user.profile:
+        if hasattr(request.user, "profile") and request.user.profile:
             serializer = self.get_serializer(data=request.data, instance=request.user.profile, partial=True)
             created_flag = False
         else:
             serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data['user'] = request.user
+        serializer.validated_data["user"] = request.user
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         response = Response(serializers.ProfileSerializer(request.user.profile).data)
@@ -76,7 +76,7 @@ class ProfileViewSet(
         return response
 
     def get_object(self):
-        if hasattr(self.request.user, 'profile') and self.request.user.profile:
+        if hasattr(self.request.user, "profile") and self.request.user.profile:
             return self.request.user.profile
         return None
 
@@ -89,7 +89,7 @@ class ProfileViewSet(
         photo = instance.photo if instance.photo else instance.photo_url
         serializer = self.get_serializer(instance)
         data = serializer.data
-        data['photo'] = photo.url if hasattr(photo, 'url') else photo
+        data["photo"] = photo.url if hasattr(photo, "url") else photo
 
         # Everytime profile retrieve gets called we update user activity.
         profile_requested.send(sender=instance.__class__, profile=instance)
@@ -143,26 +143,26 @@ class BankDetailViewSet(mixins.CreateModelMixin,
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        if hasattr(request.user, 'bank_details') and request.user.bank_details:
+        if hasattr(request.user, "bank_details") and request.user.bank_details:
             serializer = self.get_serializer(data=request.data, instance=request.user.bank_details, partial=True)
         else:
             serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data['user'] = request.user
-        stripe_token = serializer.validated_data.pop('stripe_token', None)
+        serializer.validated_data["user"] = request.user
+        stripe_token = serializer.validated_data.pop("stripe_token", None)
         if stripe_token:
             try:
                 self.get_stripe_customer_id(serializer, stripe_token)
             except:
                 raise serializers.serializers.ValidationError(
-                    {'stripe_token': _('Stripe token is not valid')}
+                    {"stripe_token": _("Stripe token is not valid")}
                 )
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(payment_serializers.BankDetailsSerializer(request.user.bank_details).data)
 
     def get_object(self):
-        if hasattr(self.request.user, 'bank_details') and self.request.user.bank_details:
+        if hasattr(self.request.user, "bank_details") and self.request.user.bank_details:
             return self.request.user.bank_details
         return None
 
@@ -184,8 +184,8 @@ class BankDetailViewSet(mixins.CreateModelMixin,
                 stripe_token
             )
         else:
-            serializer.validated_data['stripe_customer_id'] = stripe_service.get_customer_id(
-                serializer.validated_data['user'],
+            serializer.validated_data["stripe_customer_id"] = stripe_service.get_customer_id(
+                serializer.validated_data["user"],
                 stripe_token
             )
         return serializer
@@ -205,7 +205,7 @@ class LogoutView(RestLogoutView):
     def logout(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        os_id = serializer.validated_data.get('os_id', '')
+        os_id = serializer.validated_data.get("os_id", "")
         if os_id:
             try:
                 device = models.Device.objects.get(user=self.request.user, os_id=os_id)
@@ -220,11 +220,11 @@ class VerificationView(viewsets.GenericViewSet):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = serializers.NewPhoneNumberSerializer
 
-    @action(methods=['post'], detail=False, serializer_class=serializers.NewPhoneNumberSerializer)
+    @action(methods=["post"], detail=False, serializer_class=serializers.NewPhoneNumberSerializer)
     def new_phone_number(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        phone_number = serializer.validated_data.get('phone_number')
+        phone_number = serializer.validated_data.get("phone_number")
         if phone_number:
             request.user.new_phone_number = phone_number
             request.user.generate_sms_code(commit=False)
@@ -233,16 +233,16 @@ class VerificationView(viewsets.GenericViewSet):
                 phone_number,
                 messages.PHONE_CODE_VERIFICATION.format(code=request.user.sms_code)
             )
-        return Response({'status': messages.PHONE_CODE_SUCCESSFULLY_SENT})
+        return Response({"status": messages.PHONE_CODE_SUCCESSFULLY_SENT})
 
-    @action(methods=['post'], detail=False, serializer_class=serializers.CheckCodeSerializer)
+    @action(methods=["post"], detail=False, serializer_class=serializers.CheckCodeSerializer)
     def check_sms_code(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        request.user.sms_code = ''
+        request.user.sms_code = ""
         if request.user.new_phone_number:
             request.user.phone_number = request.user.new_phone_number
-            request.user.new_phone_number = ''
+            request.user.new_phone_number = ""
             phone_number_verified.send(
                 sender=self.__class__,
                 user=request.user,
@@ -251,12 +251,12 @@ class VerificationView(viewsets.GenericViewSet):
 
         request.user.set_phone_number_verified()
 
-        return Response({'status': messages.PHONE_NUMBER_SUCCESSFULLY_VERIFIED})
+        return Response({"status": messages.PHONE_NUMBER_SUCCESSFULLY_VERIFIED})
 
-    @action(methods=['post'], detail=False)
+    @action(methods=["post"], detail=False)
     def send_verify_email(self, request):
         request.user.send_verify_email()
-        return Response({'status': messages.EMAIL_VERIFY_SUCCESSFULLY_SENT})
+        return Response({"status": messages.EMAIL_VERIFY_SUCCESSFULLY_SENT})
 
 
 class UserServicesViewSet(mixins.CreateModelMixin,
@@ -270,32 +270,32 @@ class UserServicesViewSet(mixins.CreateModelMixin,
         operation_description="""
         Request example
             {
-            'years_of_experience': 'less_1_year',
-            'bar_council': 'text',
-            'followers': 100,
-            'industries': [industry_pk, industry2_pk],
-            'services': [
+            "years_of_experience": "less_1_year",
+            "bar_council": "text",
+            "followers": 100,
+            "industries": [industry_pk, industry2_pk],
+            "services": [
                 {
-                    'pk': 1, # if pk exists and service with that pk yours server update service
-                    'service_type': service_type_pk,
-                    'price_type': 'price',
-                    'price': 100,
-                    'timeline': 60,
-                    'revision': 5,
-                    'includes': 'test',
-                    'attachments': ['1','2'],
-                    'questions': ['1','2']
+                    "pk": 1, # if pk exists and service with that pk yours server update service
+                    "service_type": service_type_pk,
+                    "price_type": "price",
+                    "price": 100,
+                    "timeline": 60,
+                    "revision": 5,
+                    "includes": "test",
+                    "attachments": ["1","2"],
+                    "questions": ["1","2"]
 
                 },
                 {
-                    'service_type': service_type_pk,
-                    'price_type': 'price',
-                    'price': 100,
-                    'timeline': 60,
-                    'revision': 5,
-                    'includes': 'test',
-                    'attachments': ['1', '2'],
-                    'questions': ['1', '2']
+                    "service_type": service_type_pk,
+                    "price_type": "price",
+                    "price": 100,
+                    "timeline": 60,
+                    "revision": 5,
+                    "includes": "test",
+                    "attachments": ["1", "2"],
+                    "questions": ["1", "2"]
 
                 }
             ],
@@ -304,13 +304,13 @@ class UserServicesViewSet(mixins.CreateModelMixin,
     )
     def create(self, request, *args, **kwargs):
         created_flag = True
-        if hasattr(request.user, 'user_services_info') and request.user.user_services_info:
+        if hasattr(request.user, "user_services_info") and request.user.user_services_info:
             serializer = self.get_serializer(data=request.data, instance=request.user.user_services_info, partial=True)
             created_flag = False
         else:
             serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data['user'] = request.user
+        serializer.validated_data["user"] = request.user
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         response = Response(service_serializers.UserServicesSerializer(request.user.user_services_info).data)
@@ -324,8 +324,8 @@ class UserServicesViewSet(mixins.CreateModelMixin,
         return response
 
     def get_object(self):
-        if self.request.user.role == 'user':
-            if hasattr(self.request.user, 'user_services_info') and self.request.user.user_services_info:
+        if self.request.user.role == "user":
+            if hasattr(self.request.user, "user_services_info") and self.request.user.user_services_info:
                 return self.request.user.user_services_info
         return None
 
@@ -340,29 +340,31 @@ class UserServicesViewSet(mixins.CreateModelMixin,
         return self.retrieve(request, *args, **kwargs)
 
 
-class InvestorServicesViewSet(mixins.CreateModelMixin,
-                              mixins.ListModelMixin,
-                              viewsets.GenericViewSet):
+class InvestorServicesViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
     serializer_class = service_serializers.InvestorServicesSerializer
     queryset = service_models.InvestorServiceInfo.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        if hasattr(request.user, 'investor_services_info') and request.user.investor_services_info:
+        if hasattr(request.user, "investor_services_info") and request.user.investor_services_info:
             serializer = self.get_serializer(
                 data=request.data, instance=request.user.investor_services_info, partial=True
             )
         else:
             serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data['user'] = request.user
+        serializer.validated_data["user"] = request.user
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(service_serializers.InvestorServicesSerializer(request.user.investor_services_info).data)
 
     def get_object(self):
-        if self.request.user.role == 'investor':
-            if hasattr(self.request.user, 'investor_services_info') and self.request.user.investor_services_info:
+        if self.request.user.role == "investor":
+            if hasattr(self.request.user, "investor_services_info") and self.request.user.investor_services_info:
                 return self.request.user.investor_services_info
         return None
 
@@ -377,15 +379,17 @@ class InvestorServicesViewSet(mixins.CreateModelMixin,
         return self.retrieve(request, *args, **kwargs)
 
 
-class NetworkView(mixins.RetrieveModelMixin,
-                  mixins.ListModelMixin,
-                  GenericAPIView):
+class NetworkView(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericAPIView
+):
     serializer_class = serializers.ProfileSerializer
     pagination_class = Pagination
-    queryset = models.Profile.objects.select_related('user').all().order_by('name')
+    queryset = models.Profile.objects.select_related("user").all().order_by("name")
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    filterset_fields = ['tags']
-    search_fields = ['name']
+    filterset_fields = ["tags"]
+    search_fields = ["name"]
     permission_classes = [permissions.IsAuthenticated]
 
     def retrieve(self, request, *args, **kwargs):
@@ -397,15 +401,15 @@ class NetworkView(mixins.RetrieveModelMixin,
         if instance:
             serializer = self.get_serializer(instance)
             data = serializer.data
-            data['photo'] = photo.url if hasattr(photo, 'url') else photo
+            data["photo"] = photo.url if hasattr(photo, "url") else photo
             return Response(data)
         raise NotFound()
 
     def get(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
+        pk = kwargs.get("pk")
         if pk:
             try:
-                self.kwargs['pk'] = get_user_model().objects.get(pk=pk).profile.pk
+                self.kwargs["pk"] = get_user_model().objects.get(pk=pk).profile.pk
                 return self.retrieve(request, *args, **kwargs)
             except (get_user_model().DoesNotExist, ValidationError, Profile.DoesNotExist):
                 raise NotFound()
@@ -432,23 +436,23 @@ class RefererEmailView(APIView):
     def post(self, request):
         uuid = str(request.user.pk)
         fernet = Fernet(settings.FERNET_KEY)
-        encrypted_uuid = fernet.encrypt(uuid.encode('ascii'))
+        encrypted_uuid = fernet.encrypt(uuid.encode("ascii"))
         try:
-            email = request.data.get('email').strip()
+            email = request.data.get("email").strip()
             validate_email(email)
             if get_user_model().objects.filter(email=email).exists():
-                return Response({'email': _('Email already exists.')}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"email": _("Email already exists.")}, status=status.HTTP_400_BAD_REQUEST)
             data = {
                 email: {
-                    'key': encrypted_uuid.decode("ascii"),
-                    'user': str(request.user),
-                    'front_url': settings.FRONT_URL
+                    "key": encrypted_uuid.decode("ascii"),
+                    "user": str(request.user),
+                    "front_url": settings.FRONT_URL
                 }
             }
             send_email.delay(
-                subject=_('Signup invitation'),
+                subject=_("Signup invitation"),
                 to=[email],
-                template_name=choices.template_names.get('invite_friend'),
+                template_name=choices.template_names.get("invite_friend"),
                 content={},
                 merge_vars=data)
             referred_friend.send(
@@ -456,16 +460,18 @@ class RefererEmailView(APIView):
                 user=request.user,
                 request=request
             )
-            return Response({'detail': _('Verification e-mail sent.'), 'email': email})
+            return Response({"detail": _("Verification e-mail sent."), "email": email})
         except (ValidationError, AttributeError):
-            return Response({'email': _('Email is not valid.')}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"email": _("Email is not valid.")}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class InvestorsViewSet(mixins.ListModelMixin,
-                       mixins.RetrieveModelMixin,
-                       viewsets.GenericViewSet):
-    queryset = models.User.objects.select_related('profile').filter(
-        groups__name='Investor',
+class InvestorsViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = models.User.objects.select_related("profile").filter(
+        groups__name="Investor",
         # bank_details__isnull=False,
         investor_services_info__isnull=False,
         is_active=True,
@@ -473,20 +479,20 @@ class InvestorsViewSet(mixins.ListModelMixin,
         investor_services_info__reach_out=True,
         is_approved=True,
         profile__public_profile=True
-    ).order_by('name')
+    ).order_by("name")
 
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = Pagination
     # serializer_class = serializers.ProfileSerializer
     serializer_class = service_serializers.ProfessionalSerializer
     filterset_fields = [
-        'investor_services_info__kind_of_funding',
-        'investor_services_info__companies',
-        'profile__work_city'
+        "investor_services_info__kind_of_funding",
+        "investor_services_info__companies",
+        "profile__work_city"
     ]
 
     def get_queryset(self):
-        if getattr(self, 'swagger_fake_view', False):
+        if getattr(self, "swagger_fake_view", False):
             # queryset just for schema generation metadata
             return models.User.objects.none()
         return self.queryset.exclude(pk=self.request.user.pk)
@@ -498,14 +504,16 @@ class VerifyEmailView(DefaultVerifyEmailView):
         return serializers.VerifyEmailSerializer(*args, **kwargs)
 
 
-class CoverFileViewSet(mixins.CreateModelMixin,
-                       viewsets.GenericViewSet):
+class CoverFileViewSet(
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = models.CoverFile.objects.none()
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = serializers.CoverFileSerializer
 
     def perform_create(self, serializer):
-        serializer.validated_data['user'] = self.request.user
+        serializer.validated_data["user"] = self.request.user
         serializer.save()
 
 
@@ -515,7 +523,7 @@ class PasswordResetConfirmAPIView(DefaultPasswordResetConfirmView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid()
         data = serializer.data
-        uid = force_text(urlsafe_base64_decode(data['uid']))
+        uid = force_text(urlsafe_base64_decode(data["uid"]))
         try:
             user = models.User.objects.get(pk=uid)
         except models.User.DoesNotExist:
