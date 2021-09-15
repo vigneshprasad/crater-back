d6fc9e81cfc1f610ca7392b72d7a1759d0045652
Nishant Parashar <nishant@worknetwork.in>
2021-09-15 17:01:58 +0530

    Added versel to origin list

--- app/freelance/settings.py
+++ app/freelance/settings.py
@@ -51,6 +51,7 @@ CORS_ORIGIN_REGEX_WHITELIST = [
     r"https://[a-zA-Z-]+.1worknetwork.com*",
     r"https://[a-zA-Z-]+.worknetwork.in*",
     r"https://worknetwork.in*",
+    r"https://penitence-pre-prod.vercel.app/*"
     # r"^http://*",
 ]
 
