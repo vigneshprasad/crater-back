528e12e7d92fdd14a9e9fd87d8f1c35519054f2d
Abhishek R <abhishek@worknetwork.in>
2021-09-16 15:19:53 +0530

    Dyte Meeting viewset

---
+++ app/integrations/dyte/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework.serializers import ModelSerializer
+
+from integrations.dyte import models
+
+
+class DyteMeetingSerializer(ModelSerializer):
+
+    class Meta:
+        model = models.DyteMeeting
+        fields = (
+            "dyte_meeting_id",
+            "room_name"
+        )
+
+
+class DytePartcipantSerializer(ModelSerializer):
+
+    dyte_meeting_detail = DyteMeetingSerializer(source="dyte_meeting")
+
+    class Meta:
+        model = models.DyteMeetingParticipant
+        fields = (
+            "pk",
+            "dyte_meeting",
+            "auth_token",
+            "is_online",
+            "dyte_meeting_detail"
+        )
---
+++ app/integrations/dyte/views.py
@@ -0,0 +1,34 @@
+import logging
+
+from rest_framework import status
+from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
+from integrations.dyte import models
+from integrations.dyte import serializers
+
+from users import permissions
+
+
+class DyteParticipantViewSet(
+    mixins.RetrieveModelMixin,
+    GenericViewSet
+):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = models.DyteMeetingParticipant.objects.all()
+    serializer_class = serializers.DytePartcipantSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        user = request.user
+
+        try:
+            dyte_meeting = self.get_queryset().get(
+                participant=user,
+                dyte_meeting__group_id=pk,
+            )
+            serialized = self.get_serializer(dyte_meeting)
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        except models.DyteMeetingParticipant.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
