8469879f55f259c1b0b3995c420116836a3186e8
Nishant Parashar <nishant@worknetwork.in>
2021-09-26 23:16:07 +0530

    Minor fixes

--- app/integrations/dyte/scripts/delete_old_dyte_webhooks.py
+++ app/integrations/dyte/scripts/delete_old_dyte_webhooks.py
@@ -3,20 +3,19 @@ from integrations.dyte.service import dyte_service
 
 def run(dry_run=True):
 
-    print("Getting all Webhooks")
-    all_webhooks = dyte_service.get_all_webhooks()
-    all_webhooks_data = all_webhooks["data"]["webhooks"]
+    all_webhooks_data = dyte_service.get_all_webhooks()
 
-    print("Deleting webhooks")
-    print([webhook["id"] for webhook in all_webhooks])
+    print("Deleting webhooks with ids:")
+    print([webhook["id"] for webhook in all_webhooks_data], "\n")
 
-    for webhook in all_webhooks:
+    for webhook in all_webhooks_data:
         print("Deleting Webhook: {}".format(webhook["id"]))
 
         if not dry_run:
-            dyte_service.delete_webhook(
+            deleted = dyte_service.delete_webhook(
                 webhook["id"]
             )
-            print("Deleted Webhook: {}".format(webhook["id"]))
+            if deleted:
+                print("Deleted Webhook: {}".format(webhook["id"]))
 
         print("-----")
