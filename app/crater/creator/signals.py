c8bc31f3cf3cf17f87ef12db23eb484d1cf9687b
Nishant Parashar <nishant@worknetwork.in>
2021-09-28 19:43:07 +0530

    Follower creation on RSVP. Adding user to creator community

--- app/crater/creator/signals.py
+++ app/crater/creator/signals.py
@@ -2,3 +2,4 @@ from django.dispatch import Signal
 
 creator_followed = Signal(providing_args=["follower"])
 creator_unfollowed = Signal(providing_args=["follower"])
+user_added_to_community = Signal(providing_args=["community_member"])
