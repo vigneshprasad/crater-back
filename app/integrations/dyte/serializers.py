d979a47ddf5e037867a2fa48718ac213d50edabb
sanjeev40 <sanjeev@worknetwork.in>
2021-10-04 17:39:00 +0530

    Create DyteMeetingRecording model, admin and serializer

--- app/integrations/dyte/serializers.py
+++ app/integrations/dyte/serializers.py
@@ -4,7 +4,6 @@ from integrations.dyte import models
 
 
 class DyteMeetingSerializer(ModelSerializer):
-
     class Meta:
         model = models.DyteMeeting
         fields = (
@@ -16,7 +15,6 @@ class DyteMeetingSerializer(ModelSerializer):
 
 
 class DyteParticipantSerializer(ModelSerializer):
-
     dyte_meeting_detail = DyteMeetingSerializer(source="dyte_meeting")
 
     class Meta:
@@ -27,4 +25,20 @@ class DyteParticipantSerializer(ModelSerializer):
             "auth_token",
             "is_online",
             "dyte_meeting_detail"
+        )
+
+
+class DyteMeetingRecordingSerializer(ModelSerializer):
+    dyte_meeting_detail = DyteMeetingSerializer(source="dyte_meeting")
+
+    class Meta:
+        model = models.DyteMeetingMeetingRecording
+        fields = (
+            "pk",
+            "dyte_meeting",
+            "recording_id",
+            "status",
+            "path",
+            "started_at",
+            "stopped_at"
         )
