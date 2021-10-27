c22061b132eaa764fa0bfed56744773ad56c5cd1
sanjeev40 <sanjeev@worknetwork.in>
2021-10-27 11:34:19 +0530

    Update channel routing with custom middleware

--- app/consumers/routing.py
+++ app/consumers/routing.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from . import consumers
+# from . import consumers
+from conversations.consumers import GroupChatConsumer
 
 websocket_urlpatterns = [
-    path('ws/connector/<str:token>/', consumers.ChatConsumer),
-    path('ws/connector/<str:receiver_id>/<str:token>/', consumers.ChatConsumer),
-    path('ws/connector/webinar/<str:group_id>/<str:token>/', consumers.LiveCountConsumer),
+    path('ws/connector/group/<str:group_id>/', GroupChatConsumer),
 ]
+
+# path('ws/connector/<str:token>/', consumers.ChatConsumer),
+# path('ws/connector/<str:receiver_id>/<str:token>/', consumers.ChatConsumer),
+# path('ws/connector/webinar/<str:group_id>/<str:token>/', consumers.LiveCountConsumer),
