e6f6e7acc71f5171c19de3d3f937e7455128d29d
Vignesh Prasad <vigneshprasad@Vigneshs-MacBook-Pro.local>
2021-10-29 15:21:33 +0530

    adding test phone numbers

--- app/crater/auth/constants.py
+++ app/crater/auth/constants.py
@@ -1,5 +1,7 @@
 LOGIN_OTP_MESSAGE = "Your login code for Crater club is: {otp}"
 
+TEST_PHONE_NUMBERS = ["+919999999999"]
+
 # Adding it here because the settings one is applicable all across the
 # app. Only want debug False for crater auth.
 DEBUG = False
