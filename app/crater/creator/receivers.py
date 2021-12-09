e0f95fb52f50130e5bd6c526df8a1bd87366140a
sanjeev40 <sanjeev@worknetwork.in>
2021-12-09 13:18:30 +0530

    Set show club members to true if creator follower count reaches 50

--- app/crater/creator/receivers.py
+++ app/crater/creator/receivers.py
@@ -48,7 +48,13 @@ def update_follower_count(sender, follower, *args, **kwargs):
     """
     creator = follower.creator
     creator.follower_count = private.get_follower_count_for_creator(creator)
+
+    # Set show_club_members to true if follower count hits 50
+    if creator.follower_count >= 50 and not creator.show_club_members:
+        creator.show_club_members = True
+
     creator.save()
+
     return creator
 
 
