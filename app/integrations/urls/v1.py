755c288c1ca406abf7ccf2ce1eac88c2677e72d1
Nishant Parashar <nishant@worknetwork.in>
2022-01-28 19:11:09 +0530

    API's for Retool Data aggregation

--- app/integrations/urls/v1.py
+++ app/integrations/urls/v1.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("agora/", include("integrations.agora.urls.v1")),
-    path("dyte/", include("integrations.dyte.urls.v1"))
+    path("dyte/", include("integrations.dyte.urls.v1")),
+    path("retool/", include("integrations.retool.urls.v1")),
 ]
