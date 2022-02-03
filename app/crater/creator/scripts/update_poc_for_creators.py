59bd6993a5735ba0de50ef0e7fd48ce2094ff681
Nishant Parashar <nishant@worknetwork.in>
2022-02-03 15:25:38 +0530

    Fixed script for adding POC

--- app/crater/creator/scripts/update_poc_for_creators.py
+++ app/crater/creator/scripts/update_poc_for_creators.py
@@ -10,26 +10,37 @@ def run(
         file_url="https://1worknetwork-dev.s3.ap-south-1.amazonaws.com/data/creator_poc.csv",
         dry_run=True
 ):
+
     response = urllib_request.urlopen(file_url)
     lines = [line.decode("utf-8") for line in response.readlines()]
     reader = csv.DictReader(lines)
 
     for row in reader:
 
-        print("*"*30)
+        print("-----")
         creator_number = row.get("Creator").strip()
-        poc_number = row.get("POC").strip()
+        poc_email = row.get("POC Email").strip()
 
-        if not creator_number and poc_number:
-            print("Data not present", creator_number, poc_number)
+        if not creator_number and poc_email:
+            print("Data not present", creator_number, poc_email)
             continue
 
-        creator = models.Creator.objects.get(
-            user__username=creator_number
-        )
-        poc = get_user_model().objects.get(
-            username=poc_number
-        )
+        try:
+            creator = models.Creator.objects.get(
+                user__username=creator_number
+            )
+        except models.Creator.DoesNotExist:
+            print("Creator doesn't not exist: {}".format(creator_number))
+            continue
+
+        try:
+            poc = get_user_model().objects.get(
+                email=poc_email
+            )
+        except get_user_model().DoesNotExist:
+            print("POC doesn't not exist: {}".format(poc_email))
+            continue
+
         print("Creator: ", creator.__str__())
         print("POC: ", poc.__str__())
 
@@ -37,3 +48,5 @@ def run(
             creator.point_of_contact = poc
             creator.save()
             print("Updated POC for Creator: {}".format(creator.id))
+
+        print("-----")
