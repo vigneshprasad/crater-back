da53a75c25ffdd3efeb2a7f325a116747cc2c405
Nishant Parashar <nishant@worknetwork.in>
2022-04-05 15:16:51 +0530

    Fixed imports

--- app/users/views.py
+++ app/users/views.py
@@ -1,13 +1,8 @@
-import datetime
-from itertools import chain
-
 from cryptography.fernet import Fernet
 from django.conf import settings
 from django.contrib.auth import views as auth_views, get_user_model
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-from django.db.models import When, F, ExpressionWrapper
-from django.db.models.fields import DurationField
 from django.urls import reverse_lazy
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
@@ -16,35 +11,29 @@ from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg.utils import swagger_auto_schema
 from rest_auth.registration.views import VerifyEmailView as DefaultVerifyEmailView
 from rest_auth.utils import jwt_encode
-from rest_auth.views import PasswordResetConfirmView as DefaultPasswordResetConfirmView
-from rest_auth.views import UserDetailsView as DefaultUserDetailsView
-from rest_auth.views import LogoutView as RestLogoutView
-from rest_framework import filters
-from rest_framework import mixins, viewsets, status
+from rest_auth.views import PasswordResetConfirmView as DefaultPasswordResetConfirmView, \
+    UserDetailsView as DefaultUserDetailsView, LogoutView as RestLogoutView
+from rest_framework import filters, mixins, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from conversations import models as conversation_models
 from payment import models as payment_models, serializers as payment_serializers
 from payment.tasks import charge_subscription_payment
-from services import serializers as service_serializers, models as service_models
-from users import permissions
-from users import services
-from users import utils
+from resources.meetings import models as meeting_models
+from users import permissions, services, utils
 from utils import messages
 from utils.stripe_service import stripe_service
 from . import serializers, models, constants
 from .forms import AdminSetPasswordForm
 from .models import Profile
 from .paginators import Pagination
-from .signals import basic_profile_created, service_created, phone_number_verified, referred_friend, profile_requested
+from .signals import basic_profile_created, phone_number_verified, referred_friend, profile_requested
 from .swagger_schemas import referer_email
 from .tasks import send_email
-from resources.meetings import models as meeting_models
-from conversations import models as conversation_models
-from integrations.dyte import models as dyte_models
 
 
 class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
