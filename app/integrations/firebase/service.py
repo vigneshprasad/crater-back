f22e52243314c3d12c0aa0f16b0b249c8bbc899e
sanjeev40 <sanjeev@worknetwork.in>
2022-04-27 14:45:04 +0530

    Add firebase service functions:
    - Custom registration to register admin in backend
    - Send message
    - Get document

--- app/integrations/firebase/service.py
+++ app/integrations/firebase/service.py
@@ -1,4 +1,5 @@
 import firebase_admin
+import requests
 
 from firebase_admin import auth
 from firebase_admin import credentials
@@ -35,6 +36,22 @@ class FirebaseService:
         )
         return token
 
+    @staticmethod
+    def custom_registration(email, username, user_pk):
+        """Register user to Firebase DB."""
+        additional_claims = {
+            "email": email,
+            "username": username,
+        }
+        uuid = str(user_pk)
+        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
+            uuid = settings.ENVIRONMENT + "_" + uuid
+        token = auth.create_custom_token(
+            uuid,
+            additional_claims
+        )
+        return token
+
     def set_document(self, document_id, collection, data):
         """Set a document on Firebase DB."""
         if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
@@ -42,6 +59,33 @@ class FirebaseService:
         ref = self.db.collection(collection).document(document_id)
         updated = ref.set(data, merge=True)
         return updated
+
+    @staticmethod
+    def send_message(data, group_id, sender):
+        data["group"] = group_id
+        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
+            data["group"] = settings.ENVIRONMENT + "_" + group_id
+
+        uuid = str(sender)
+        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
+            uuid = settings.ENVIRONMENT + "_" + uuid
+
+        data["sender"] = uuid
+
+        resp = requests.post(
+            "https://us-central1-crater-b6a7b.cloudfunctions.net/sendMessage",
+            data
+        )
+
+        return resp.text
+
+    def get_document(self, document_id, collection):
+        """Get a user by email from document on Firebase DB."""
+        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
+            document_id = settings.ENVIRONMENT + "_" + document_id
+        ref = self.db.collection(collection).document(document_id)
+        document = ref.get()
+        return document
 
 
 firebase_service = FirebaseService(
