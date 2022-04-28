a55fa6511a67fe6cbf917bea34fc0736eb9c6eaf
sanjeev40 <sanjeev@worknetwork.in>
2022-04-28 15:26:24 +0530

    Update firebase service class:
    - Add static method to get value by environment
    - Add send message API endpoint as a class constant

--- app/integrations/firebase/service.py
+++ app/integrations/firebase/service.py
@@ -14,6 +14,10 @@ class FirebaseService:
     app = None
     db = None
 
+    FIREBASE_API_ENDPOINTS = {
+        "send_message": "https://us-central1-crater-b6a7b.cloudfunctions.net/sendMessage"
+    }
+
     def __init__(self, config):
         """Initialise firebase connection with our config."""
         cred = credentials.Certificate(config)
@@ -21,31 +25,33 @@ class FirebaseService:
         self.db = firestore.client()
 
     @staticmethod
-    def register(user):
+    def get_value_by_env(value):
+        """Return value based on environment."""
+        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
+            value = settings.ENVIRONMENT + "_" + value
+
+        return value
+
+    def register(self, user):
         """Register user to Firebase DB."""
         additional_claims = {
             "email": user.email,
             "username": user.username,
         }
-        uuid = str(user.pk)
-        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
-            uuid = settings.ENVIRONMENT + "_" + uuid
+        uuid = str(self.get_value_by_env(user.pk))
         token = auth.create_custom_token(
           uuid,
           additional_claims
         )
         return token
 
-    @staticmethod
-    def custom_registration(email, username, user_pk):
+    def custom_registration(self, email, username, user_pk):
         """Register user to Firebase DB."""
         additional_claims = {
             "email": email,
             "username": username,
         }
-        uuid = str(user_pk)
-        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
-            uuid = settings.ENVIRONMENT + "_" + uuid
+        uuid = str(self.get_value_by_env(user_pk))
         token = auth.create_custom_token(
             uuid,
             additional_claims
@@ -54,26 +60,17 @@ class FirebaseService:
 
     def set_document(self, document_id, collection, data):
         """Set a document on Firebase DB."""
-        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
-            document_id = settings.ENVIRONMENT + "_" + document_id
+        document_id = str(self.get_value_by_env(document_id))
         ref = self.db.collection(collection).document(document_id)
         updated = ref.set(data, merge=True)
         return updated
 
-    @staticmethod
-    def send_message(data, group_id, sender):
-        data["group"] = str(group_id)
-        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
-            data["group"] = settings.ENVIRONMENT + "_" + group_id
-
-        uuid = str(sender)
-        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
-            uuid = settings.ENVIRONMENT + "_" + uuid
-
-        data["sender"] = uuid
+    def send_message(self, data, group_id, sender):
+        data["group"] = str(self.get_value_by_env(group_id))
+        data["sender"] = str(self.get_value_by_env(sender))
 
         resp = requests.post(
-            "https://us-central1-crater-b6a7b.cloudfunctions.net/sendMessage",
+            self.FIREBASE_API_ENDPOINTS["send_message"],
             data
         )
 
@@ -81,8 +78,7 @@ class FirebaseService:
 
     def get_document(self, document_id, collection):
         """Get a user by email from document on Firebase DB."""
-        if settings.ENVIRONMENT != settings.ENVIRONMENT_PROD:
-            document_id = settings.ENVIRONMENT + "_" + document_id
+        document_id = str(self.get_value_by_env(document_id))
         ref = self.db.collection(collection).document(document_id)
         document = ref.get()
         return document
