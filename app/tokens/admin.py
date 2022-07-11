d53521987de65fe97fd12868a8f2ee96f4cb1477
Nishant Parashar <nishant@worknetwork.in>
2022-07-11 17:47:26 +0530

    Added admin for tokens

--- app/tokens/admin.py
+++ app/tokens/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from tokens import models
+
+
+@admin.register(models.TokenDataPerDay)
+class TokenDataPerDayAdmin(admin.ModelAdmin):
+    exclude = ("created_at", "deleted_at", "updated_at", "is_deleted")
+
+
+@admin.register(models.TokenTransaction)
+class TokenTransactionAdmin(admin.ModelAdmin):
+    exclude = ("created_at", "deleted_at", "updated_at", "is_deleted")
+
+
+@admin.register(models.UserTokenLog)
+class UserTokenLogAdmin(admin.ModelAdmin):
+    exclude = ("created_at", "deleted_at", "updated_at", "is_deleted")
