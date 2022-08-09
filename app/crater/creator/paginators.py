8835ebda3f4261900a4e4ee3e66400c4eaea0639
sanjeev40 <sanjeev@worknetwork.in>
2022-08-09 15:17:05 +0530

    Update follower pagination page size

--- app/crater/creator/paginators.py
+++ app/crater/creator/paginators.py
@@ -19,7 +19,7 @@ class CreatorPagination(PageNumberPagination):
 
 
 class FollowerPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 10
     page_size_query_param = "page_size"
 
     def get_paginated_response(self, data):
