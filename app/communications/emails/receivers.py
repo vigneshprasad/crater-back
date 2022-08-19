1bb40e46d123cbffe5570053bce17940b826d34e
Nishant Parashar <nishant@worknetwork.in>
2022-08-19 16:07:23 +0530

    Check for group start for published email

--- app/communications/emails/receivers.py
+++ app/communications/emails/receivers.py
@@ -1,4 +1,5 @@
 from django.dispatch import receiver
+from django.utils import timezone
 
 from communications.emails import constants, private, tasks
 from conversations import signals as conversations_signals, public as conversations_public
@@ -39,13 +40,16 @@ def send_email_to_creator_for_recording_published(sender, recording, *args, **kw
     )
 
 
-@receiver(creator_signals.creator_followed)
-def send_email_for_50_subscribers_to_creator(sender, follower, *args, **kwargs):
-    creator = follower.creator
-    subscriber_count = creator_public.get_subscriber_count_for_creator(creator)
-    if subscriber_count != 50:
-        return
+@receiver(creator_signals.creator_50_subscribers)
+def send_email_for_50_subscribers_to_creator(sender, creator, *args, **kwargs):
+    """Sends email once a creators has 50 subscribers.
 
+    Args:
+        sender(Creator.__class__): Class repr of creator that has reached
+            50 subscribers.
+        creator(Group): Creator that has reached 50 subscribers.
+
+    """
     private.send_email_for_user(
         subject="You have gained 50 followers on Crater!",
         user=creator.user,
@@ -57,6 +61,26 @@ def send_email_for_50_subscribers_to_creator(sender, follower, *args, **kwargs):
 
 @receiver(conversations_signals.group_marked_published)
 def send_email_for_stream_setup_to_creator(sender, group, *args, **kwargs):
+    """Sends email once a creators stream is set up on the platform.
+
+    Args:
+        sender(Group.__class__): Class repr of group that was published.
+        group(Group): Group that was marked published.
+
+    """
+    group_start = group.start
+    now_time = timezone.now()
+    # Don't send the email if the group start is less than now time.
+    if group_start <= now_time:
+        return
+
+    diff = now_time - group_start
+    diff_minutes = diff.seconds / 60
+    # If the group is marked published within 30 minutes of group start
+    # don't send the published email.
+    if diff_minutes < 30:
+        return
+
     tasks.send_stream_setup_email_to_creator.apply_async(
         args=(group.id, ),
         countdown=120
--- app/crater/creator/signals.py
+++ app/crater/creator/signals.py
@@ -3,3 +3,4 @@ from django.dispatch import Signal
 creator_followed = Signal(providing_args=["follower"])
 creator_unfollowed = Signal(providing_args=["follower"])
 user_added_to_community = Signal(providing_args=["community_member"])
+creator_50_subscribers = Signal(providing_args=["creator"])
