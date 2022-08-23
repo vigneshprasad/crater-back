67d39e232aad5c1ad4a2cdf26450ca7ae39e8da5
Nishant Parashar <nishant@worknetwork.in>
2022-08-23 16:56:26 +0530

    Fixed tokens task

--- app/tokens/tasks.py
+++ app/tokens/tasks.py
@@ -49,6 +49,20 @@ def calculate_tokens_earned(date=None):
         total_minutes=Sum("total_minutes_spent_by_attendees")
     )["total_minutes"] or 0
 
+    # New calculation for total watch time.
+    total_watch_time = 0
+
+    for stream in streams_for_today:
+        dyte_participants = dyte_models.DyteMeetingParticipant.objects.filter(
+            dyte_meeting__group=stream,
+            last_online_at__isnull=False,
+        ).exclude(
+            participant=stream.host
+        )
+        for dyte_participant in dyte_participants:
+            total_watch_time += dyte_participant.total_minutes_watched
+
+    # TODO(Nishant): Should we filter out prompts etc.
     total_engagement = conversations_models.GroupMessage.objects.filter(
         group__in=streams_for_today,
         created_at__gte=today_start,
@@ -68,9 +82,19 @@ def calculate_tokens_earned(date=None):
     for stream in streams_for_today:
         # Calculate tokens distributed for the streamer first.
         host = stream.host
-        streamer_time_spent = stream.total_minutes_spent_by_attendees
+        host_dyte_participant = dyte_models.DyteMeetingParticipant.objects.filter(
+            dyte_meeting__group=stream,
+            participant=stream.host,
+            last_online_at__isnull=False
+        ).last()
+        if not host_dyte_participant:
+            continue
+
+        streamer_time_spent = host_dyte_participant.total_minutes_watched
         if not streamer_time_spent:
             continue
+
+        # TODO(Nishant): Should we exclude creator messages from this.
         streamer_engagement = conversations_models.GroupMessage.objects.filter(
             group=stream
         ).count()
