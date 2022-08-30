061cba586f0d10c9aede48a2e09e9bc860f3ba2b
Nishant Parashar <nishant@worknetwork.in>
2022-08-30 15:50:00 +0530

    Leaderboard addition script

--- app/leaderboard/scripts/add_creators_to_leaderboard.py
+++ app/leaderboard/scripts/add_creators_to_leaderboard.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.contrib.auth import get_user_model
 from django.db.models import Sum
 
@@ -7,7 +5,7 @@ from conversations import models as conversation_models
 from leaderboard import models, tasks
 
 
-def run(leaderboard_id, start_date=None, end_date=None, dry_run=True):
+def run(leaderboard_id, max_users=5, start_date=None, end_date=None, dry_run=True):
 
     leaderboard = models.Leaderboard.objects.get(id=leaderboard_id)
     if not end_date:
@@ -26,6 +24,7 @@ def run(leaderboard_id, start_date=None, end_date=None, dry_run=True):
 
     print("Adding creators to leaderboard: {}".format(leaderboard))
     leaderboard_participants = []
+    leaderboard_participants_dict = {}
 
     print("Creators with 2+ streams in the duration.")
     for host in hosts:
@@ -45,14 +44,32 @@ def run(leaderboard_id, start_date=None, end_date=None, dry_run=True):
         if minutes_watched < 1000:
             continue
 
+        # Append the user and minutes to a dict with
+        # user.pk as key and minutes as value.
         user = get_user_model().objects.get(pk=host)
-        print(user)
-        print(group_count)
-        print(minutes_watched)
-        print("-"*30)
-        leaderboard_participants.append(user)
+        if leaderboard_participants_dict.get(user.pk):
+            leaderboard_participants_dict[user.pk] += minutes_watched
+        else:
+            leaderboard_participants_dict[user.pk] = minutes_watched
+
+    # Reverse sort the dict of user and minutes
+    leaderboard_participants_sorted_dict = dict(sorted(
+        leaderboard_participants_dict.items(),
+        key=lambda item: item[1],
+        reverse=True
+    ))
+
+    for user_pk, minutes in leaderboard_participants_sorted_dict.items():
+        user = get_user_model().objects.get(pk=user_pk)
+        if len(leaderboard_participants) < max_users:
+            print(user)
+            print(groups.filter(host=user).count())
+            print(minutes)
+            print("-" * 30)
+            leaderboard_participants.append(user)
 
     print("Adding {} participants to Leaderboard: {}".format(len(leaderboard_participants), leaderboard))
+
     if not dry_run:
         leaderboard.participants.add(*leaderboard_participants)
         print("Added all participants")
