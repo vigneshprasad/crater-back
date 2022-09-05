ae269c983d4853fb5456011f7199221338b8ae8a
Nishant Parashar <nishant@worknetwork.in>
2022-09-05 15:58:12 +0530

    Added logging for start and stop time from Dyte

--- app/integrations/dyte/views.py
+++ app/integrations/dyte/views.py
@@ -285,7 +285,8 @@ class DyteMeetingRecordingViewSet(
             dyte_meeting_recording.stopped_at = datetime.datetime.strptime(
                 stopped_at, constants.DYTE_DATETIME_FORMAT
             ) if stopped_at else None
-        except ValueError:
+        except ValueError as e:
+            LOGGER.error("Recording times errored: {}".format(e))
             dyte_meeting_recording.started_at = None
             dyte_meeting_recording.stopped_at = None
 
