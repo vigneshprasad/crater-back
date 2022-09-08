551dcf280e6ca5be1aa0c5f41dd833770b4d1cac
Nishant Parashar <nishant@worknetwork.in>
2022-09-08 18:10:46 +0530

    Minor fixes to script

--- app/tokens/scripts/recalculate_minutes_and_tokens_for_date.py
+++ app/tokens/scripts/recalculate_minutes_and_tokens_for_date.py
@@ -1,3 +1,8 @@
+import datetime
+
+import pytz
+from django.conf import settings
+
 from conversations import models as conversation_models
 from integrations.dyte import tasks as dyte_tasks
 from tokens import tasks
@@ -5,15 +10,24 @@ from tokens import tasks
 
 def run(date, dry_run=True):
 
-    group_ids_for_the_date = conversation_models.Group.objects.filter(
-        start__gte=date,
-        start__lte=date
-    ).values_list("id", flat=True)
+    today = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    today_start = datetime.datetime.combine(today, datetime.time())
+    today_end = datetime.datetime.combine(today, datetime.time(23, 59))
+    # Make datetime timezone aware.
+    timezone = pytz.timezone(settings.TIME_ZONE)
+    today_start = timezone.localize(today_start)
+    today_end = timezone.localize(today_end)
 
-    print("Recalculating minutes for {} groups".format(len(group_ids_for_the_date)))
-    print(group_ids_for_the_date)
+    # Only get streams whose hosts are eligible for learn tokens.
+    streams_for_date = conversation_models.Group.objects.filter(
+        start__gte=today_start,
+        end__lte=today_end
+    )
+
+    print("Recalculating minutes for {} groups".format(len(streams_for_date)))
+    print(streams_for_date)
     if not dry_run:
-        dyte_tasks.recalculate_minutes_for_groups(group_ids=group_ids_for_the_date)
+        dyte_tasks.recalculate_minutes_for_groups(group_ids=streams_for_date.values_list("id", flat=True))
         print("Recalculated minutes")
 
     print("Updating tokens for date: {}".format(date))
