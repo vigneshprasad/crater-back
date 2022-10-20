479117b4844669357e07be743b7bf51c4be6bab9
Nishant Parashar <nishant@worknetwork.in>
2022-10-20 16:18:16 +0530

    Added created at filter to phone opt admin

--- app/crater/auth/admin.py
+++ app/crater/auth/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rangefilter import filter
 
 from crater.auth import models
 
@@ -8,6 +9,14 @@ class PhoneOtpAdmin(admin.ModelAdmin):
 
     list_display = ("id", "user", "phone_number", "otp", "used", "is_expired", "created_at")
     raw_id_fields = ("user", )
-    list_filter = ("used", )
+    list_filter = (
+        ("created_at", filter.DateRangeFilter),
+        "used",
+    )
     search_fields = ("user__username", "user__name", "user__email", "phone_number")
     exclude = ("deleted_at", "updated_at", "is_deleted")
+
+    @staticmethod
+    def get_rangefilter_created_at_title(request, field_path="start"):
+        """Returns the title for the start date filter."""
+        return "Sent at"
