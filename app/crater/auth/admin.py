53d273426e7c885009858dee17445c7ff92f17bd
Nishant Parashar <nishant@worknetwork.in>
2022-10-20 16:13:22 +0530

    Twilio account switch

--- app/crater/auth/admin.py
+++ app/crater/auth/admin.py
@@ -5,7 +5,8 @@ from crater.auth import models
 
 @admin.register(models.PhoneOtp)
 class PhoneOtpAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "phone_number", "otp", "used", "is_expired")
+
+    list_display = ("id", "user", "phone_number", "otp", "used", "is_expired", "created_at")
     raw_id_fields = ("user", )
     search_fields = ("user__username", "user__name", "user__email", "phone_number")
-    exclude = ("created_at", "deleted_at", "updated_at", "is_deleted")
+    exclude = ("deleted_at", "updated_at", "is_deleted")
